4983ac0acb8aaf02362ea9197916565961737804
JuHyeongKim99 <90828283+JuHyeongKim99@users.noreply.github.com>
2022-06-13 17:43:19 +0900

    get time

---
+++ app/src/main/java/com/gachon/wifiindoorpositioning/wifiindoorpositioning/ui/UnifiedNavigationActivity.java
@@ -0,0 +1,23 @@
+package com.gachon.wifiindoorpositioning.wifiindoorpositioning.ui;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+
+import com.gachon.wifiindoorpositioning.wifiindoorpositioning.ui.frags.PrefsFragment;
+
+/**
+ * Created by suyashg on 10/09/17.
+ */
+
+public class UnifiedNavigationActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new PrefsFragment()).commit();
+
+    }
+}
